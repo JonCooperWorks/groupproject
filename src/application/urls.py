676b8a9ffa9d83d2a8f4baf6a0cacc13addf5be1
Jonathan Cooper <jonathan.cooper.2010@gmail.com>
2014-03-25 03:12:25 -0500

    Removed student and lecturer home routes

--- src/application/urls.py
+++ src/application/urls.py
@@ -17,12 +17,6 @@ app.add_url_rule('/_ah/warmup', 'warmup', view_func=views.warmup)
 # Home page
 app.add_url_rule('/', 'home', view_func=views.home)
 
-# Student home page
-app.add_url_rule('/studenthome', 'studenthome', view_func=views.studenthome)
-
-# Lecturer home page
-app.add_url_rule('/lecturerhome', 'lecturerhome', view_func=views.lecturerhome)
-
 # Login Page
 app.add_url_rule(
     '/login', 'login', view_func=views.login, methods=['GET', 'POST'])
@@ -52,6 +46,7 @@ app.add_url_rule('/studenttestview', 'studenttestview',
 app.add_url_rule('/lecturertestview', 'lecturertestview',
                  view_func=views.lecturertestview)
 app.add_url_rule('/populate', 'populate', view_func=views.populate)
+
 
 # Error handlers
 # Handle 404 errors
