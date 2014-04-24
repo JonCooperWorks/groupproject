adfa37174ad0921c0a831eb4bdd51ba0212234e8
Kevin Leyow <kleyow@gmail.com>
2014-04-23 21:05:21 -0500

    Filled in analysis tables and averages for closed questions.

---
+++ src/application/static/js/keen.js
@@ -0,0 +1,7 @@
+    var Keen=Keen||{configure:function(e){this._cf=e},addEvent:function(e,t,n,i){this._eq=this._eq||[],this._eq.push([e,t,n,i])},setGlobalProperties:function(e){this._gp=e},onChartsReady:function(e){this._ocrq=this._ocrq||[],this._ocrq.push(e)}};(function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src=("https:"==document.location.protocol?"https://":"http://")+"dc8na2hxrj29i.cloudfront.net/code/keen-2.1.0-min.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})();
+
+    Keen.configure({
+        projectId: "5343e251d97b85610e00005c",
+        readKey: "26d63e407798e31ae1f114ce7525495a8bbabfe0253d4f6c4ee8d26a83b210157a4b8971f08ab16aba407acf48ee28492dd96a9a6dce124f6853073293080a7f1c39b6783346bb5e9e1ecdf867159d08c089fd15eb4272398e4307fc8474f5ec32850ba1255a1d53b0cd486f75f01a13",
+        writeKey: "c7ff13a09bd60bfa3ced64ab963467eaf4cf087a9a032a2704b28813fbe14a9e47e5f3e888fbd3ecc0120708b894e74941dcab0a75751e7fd18f22b86cdd54797e553a8583bfb5751275b2b4668d0ce0ccd0b3a1f71c3a8d54369e5000b3ce6f090a64d2c88d10ec214e1bc4bf15b806"
+    });
